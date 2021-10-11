91bc4ebcba5534fdbb3b6a6fcf4f7a838b4adafb
BrownBear85 <91921122+BrownBear85@users.noreply.github.com>
2021-10-11 09:00:52 -0700

    bismuth block explosion is much better now

--- src/main/java/net/mcreator/filler/procedures/BismuthBlockRedstoneOnProcedure.java
+++ src/main/java/net/mcreator/filler/procedures/BismuthBlockRedstoneOnProcedure.java
@@ -1,9 +1,5 @@
 package net.mcreator.filler.procedures;
 
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.common.MinecraftForge;
-
 import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
@@ -23,6 +19,7 @@ import net.minecraft.block.Blocks;
 import net.mcreator.filler.FillerMod;
 
 import java.util.function.Function;
+import java.util.Random;
 import java.util.Map;
 import java.util.Comparator;
 
@@ -52,57 +49,73 @@ public class BismuthBlockRedstoneOnProcedure {
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-		new Object() {
-			private int ticks = 0;
-			private float waitTicks;
-			private IWorld world;
-			public void start(IWorld world, int waitTicks) {
-				this.waitTicks = waitTicks;
-				MinecraftForge.EVENT_BUS.register(this);
-				this.world = world;
-			}
-
-			@SubscribeEvent
-			public void tick(TickEvent.ServerTickEvent event) {
-				if (event.phase == TickEvent.Phase.END) {
-					this.ticks += 1;
-					if (this.ticks >= this.waitTicks)
-						run();
-				}
-			}
-
-			private void run() {
+		double direction = 0;
+		if (world instanceof ServerWorld) {
+			LightningBoltEntity _ent = EntityType.LIGHTNING_BOLT.create((World) world);
+			_ent.moveForced(Vector3d.copyCenteredHorizontally(new BlockPos((int) x, (int) y, (int) z)));
+			_ent.setEffectOnly(false);
+			((World) world).addEntity(_ent);
+		}
+		world.setBlockState(new BlockPos((int) x, (int) y, (int) z), Blocks.AIR.getDefaultState(), 3);
+		for (int index0 = 0; index0 < (int) (250); index0++) {
+			direction = (double) ((new Random()).nextInt((int) 3 + 1));
+			if ((direction == 0)) {
 				if (world instanceof ServerWorld) {
-					LightningBoltEntity _ent = EntityType.LIGHTNING_BOLT.create((World) world);
-					_ent.moveForced(Vector3d.copyCenteredHorizontally(new BlockPos((int) x, (int) y, (int) z)));
-					_ent.setEffectOnly(false);
-					((World) world).addEntity(_ent);
+					Entity entityToSpawn = new TNTEntity(EntityType.TNT, (World) world);
+					entityToSpawn.setLocationAndAngles(x, y, z, (float) 0, (float) 0);
+					entityToSpawn.setRenderYawOffset((float) 0);
+					entityToSpawn.setRotationYawHead((float) 0);
+					entityToSpawn.setMotion(Math.random(), Math.random(), Math.random());
+					if (entityToSpawn instanceof MobEntity)
+						((MobEntity) entityToSpawn).onInitialSpawn((ServerWorld) world, world.getDifficultyForLocation(entityToSpawn.getPosition()),
+								SpawnReason.MOB_SUMMONED, (ILivingEntityData) null, (CompoundNBT) null);
+					world.addEntity(entityToSpawn);
 				}
-				world.setBlockState(new BlockPos((int) x, (int) y, (int) z), Blocks.AIR.getDefaultState(), 3);
-				for (int index0 = 0; index0 < (int) (100); index0++) {
-					if (world instanceof ServerWorld) {
-						Entity entityToSpawn = new TNTEntity(EntityType.TNT, (World) world);
-						entityToSpawn.setLocationAndAngles(x, y, z, (float) 0, (float) 0);
-						entityToSpawn.setRenderYawOffset((float) 0);
-						entityToSpawn.setRotationYawHead((float) 0);
-						entityToSpawn.setMotion(Math.random(), Math.random(), Math.random());
-						if (entityToSpawn instanceof MobEntity)
-							((MobEntity) entityToSpawn).onInitialSpawn((ServerWorld) world,
-									world.getDifficultyForLocation(entityToSpawn.getPosition()), SpawnReason.MOB_SUMMONED, (ILivingEntityData) null,
-									(CompoundNBT) null);
-						world.addEntity(entityToSpawn);
-					}
-					((Entity) world
-							.getEntitiesWithinAABB(TNTEntity.class,
-									new AxisAlignedBB(x - (1 / 2d), y - (1 / 2d), z - (1 / 2d), x + (1 / 2d), y + (1 / 2d), z + (1 / 2d)), null)
-							.stream().sorted(new Object() {
-								Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
-									return Comparator.comparing((Function<Entity, Double>) (_entcnd -> _entcnd.getDistanceSq(_x, _y, _z)));
-								}
-							}.compareDistOf(x, y, z)).findFirst().orElse(null)).getPersistentData().putDouble("Fuse", 60);
+			} else if ((direction == 1)) {
+				if (world instanceof ServerWorld) {
+					Entity entityToSpawn = new TNTEntity(EntityType.TNT, (World) world);
+					entityToSpawn.setLocationAndAngles(x, y, z, (float) 0, (float) 0);
+					entityToSpawn.setRenderYawOffset((float) 0);
+					entityToSpawn.setRotationYawHead((float) 0);
+					entityToSpawn.setMotion((Math.random() * (-1)), Math.random(), Math.random());
+					if (entityToSpawn instanceof MobEntity)
+						((MobEntity) entityToSpawn).onInitialSpawn((ServerWorld) world, world.getDifficultyForLocation(entityToSpawn.getPosition()),
+								SpawnReason.MOB_SUMMONED, (ILivingEntityData) null, (CompoundNBT) null);
+					world.addEntity(entityToSpawn);
 				}
-				MinecraftForge.EVENT_BUS.unregister(this);
+			} else if ((direction == 2)) {
+				if (world instanceof ServerWorld) {
+					Entity entityToSpawn = new TNTEntity(EntityType.TNT, (World) world);
+					entityToSpawn.setLocationAndAngles(x, y, z, (float) 0, (float) 0);
+					entityToSpawn.setRenderYawOffset((float) 0);
+					entityToSpawn.setRotationYawHead((float) 0);
+					entityToSpawn.setMotion(Math.random(), Math.random(), (Math.random() * (-1)));
+					if (entityToSpawn instanceof MobEntity)
+						((MobEntity) entityToSpawn).onInitialSpawn((ServerWorld) world, world.getDifficultyForLocation(entityToSpawn.getPosition()),
+								SpawnReason.MOB_SUMMONED, (ILivingEntityData) null, (CompoundNBT) null);
+					world.addEntity(entityToSpawn);
+				}
+			} else if ((direction == 3)) {
+				if (world instanceof ServerWorld) {
+					Entity entityToSpawn = new TNTEntity(EntityType.TNT, (World) world);
+					entityToSpawn.setLocationAndAngles(x, y, z, (float) 0, (float) 0);
+					entityToSpawn.setRenderYawOffset((float) 0);
+					entityToSpawn.setRotationYawHead((float) 0);
+					entityToSpawn.setMotion((Math.random() * (-1)), Math.random(), (Math.random() * (-1)));
+					if (entityToSpawn instanceof MobEntity)
+						((MobEntity) entityToSpawn).onInitialSpawn((ServerWorld) world, world.getDifficultyForLocation(entityToSpawn.getPosition()),
+								SpawnReason.MOB_SUMMONED, (ILivingEntityData) null, (CompoundNBT) null);
+					world.addEntity(entityToSpawn);
+				}
 			}
-		}.start(world, (int) 20);
+			((Entity) world
+					.getEntitiesWithinAABB(TNTEntity.class,
+							new AxisAlignedBB(x - (1 / 2d), y - (1 / 2d), z - (1 / 2d), x + (1 / 2d), y + (1 / 2d), z + (1 / 2d)), null)
+					.stream().sorted(new Object() {
+						Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
+							return Comparator.comparing((Function<Entity, Double>) (_entcnd -> _entcnd.getDistanceSq(_x, _y, _z)));
+						}
+					}.compareDistOf(x, y, z)).findFirst().orElse(null)).getPersistentData().putDouble("Fuse", 60);
+		}
 	}
 }
